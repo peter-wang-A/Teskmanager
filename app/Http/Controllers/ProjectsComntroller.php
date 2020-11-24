921349d178d3f729e22ce8e0cfcd37ccc8c6eec9
vagrant <vagrant@homestead>
2020-11-24 14:18:35 +0000

    路由

---
+++ app/Http/Controllers/ProjectsComntroller.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProjectsComntroller extends Controller
+{
+    public function store()
+    {
+        return '访问到了';
+    }
+}
